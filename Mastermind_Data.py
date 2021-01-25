4baa7756b64de77c5fcaa5354465f142a38cb213
GZimmer <kristianz1000@gmail.com>
2021-01-25 09:01:55 +0100

    update

--- Mastermind_Data.py
+++ Mastermind_Data.py
@@ -1,2 +1,4 @@
+def gemTing():
+    pass
 
 
--- Mastermind_Logic.py
+++ Mastermind_Logic.py
@@ -1,1 +1,30 @@
+import Mastermind_Data
+
+class dinmor:
+    dict = {
+        "blå": 0,
+        "grøn": 1,
+        "gul": 2,
+    }
+
+    def huskFarve(self):
+        gæt = ["blå", "blå", "blå"]
+        return gæt
+
+    def tjekFarve(self, gæt : tuple, metode = huskFarve()):
+        rigtig = metode()
+        for x in range(len(rigtig)):
+            if rigtig[x] == gæt[x]:
+                print("yes")
+            else:
+                print("no")
+
+
+    def GenereFarve(self):
+        pass
+
+
+gæt = ["blå", "grøn", "blå"]
+
+dinmor().tjekFarve(gæt)
 
