2ecea183998bb2b47997512ee296c31385fdef19
nightvobs <74359672+nightvobs@users.noreply.github.com>
2021-02-01 10:03:43 +0100

    feck

--- Mastermind_present.py
+++ Mastermind_present.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+
 gui = Tk()
 gui.title("MustardMind")
 gui.geometry("1000x500")
@@ -8,22 +9,42 @@ for i in range(10):
 for i in range(4):
     gui.rowconfigure(i, weight=2)
 for i in range(2):
-    gui.rowconfigure(i+4, weight=1)
-
-
+    gui.rowconfigure(i + 4, weight=1)
 
 backg = Label(gui, bg="grey")
-backg.grid(column=0, row=0, columnspan=10, rowspan=4,sticky="NESW")
+backg.grid(column=0, row=0, columnspan=10, rowspan=4, sticky="NESW")
 UI = Label(gui, bg="black")
 UI.grid(column=0, row=4, columnspan=10, rowspan=2, sticky="NESW")
 
-green = Button(gui, bg="dark green")
+
+GuessLst = []
+def AddColor(n):
+    GuessLst.append(n)
+    print(GuessLst)
+def AddGreen():
+    AddColor("dark green")
+def AddRed():
+    AddColor("dark red")
+def AddBlue():
+    AddColor("dark blue")
+def AddYellow():
+    AddColor("Yellow")
+def AddPurple():
+    AddColor("purple")
+def AddWhite():
+    AddColor("white")
+
+green = Button(gui, bg="dark green", command=AddGreen)
 green.grid(column=0, row=4, sticky="NESW")
-red = Button(gui, bg="dark red")
+red = Button(gui, bg="dark red", command=AddRed)
 red.grid(column=1, row=4, sticky="NESW")
-yellow = Button(gui, bg="yellow")
+yellow = Button(gui, bg="yellow", command=AddYellow)
 yellow.grid(column=1, row=5, sticky="NESW")
-blue = Button(gui, bg="dark blue")
+blue = Button(gui, bg="dark blue", command=AddBlue)
 blue.grid(column=0, row=5, sticky="NESW")
+purple = Button(gui, bg="purple", command=AddPurple)
+purple.grid(column=2, row=5, sticky="NESW")
+white = Button(gui, bg="white", command=AddWhite)
+white.grid(column=2, row=4, sticky="NESW")
 
 gui.mainloop()
--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 from tkinter import *
 
-top = Tk()
+OPTIONS = ["Jan", "Feb", "Mar"]  # etc
 
-B1 = Button(top, text ="FLAT", relief=FLAT )
-B2 = Button(top, text ="RAISED", relief=RAISED )
-B3 = Button(top, text ="SUNKEN", relief=SUNKEN )
-B4 = Button(top, text ="GROOVE", relief=GROOVE )
-B5 = Button(top, text ="RIDGE", relief=RIDGE )
+master = Tk()
 
-B1.pack()
-B2.pack()
-B3.pack()
-B4.pack()
-B5.pack()
-top.mainloop()
+variable = StringVar(master)
+variable.set(OPTIONS[0])  # default value
+
+w = OptionMenu(master, variable, *OPTIONS)
+w.pack()
+
+mainloop()
