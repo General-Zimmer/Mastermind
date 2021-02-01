4142a368e55664faf14123ea589418f8f4cd39b9
nightvobs <74359672+nightvobs@users.noreply.github.com>
2021-02-01 09:06:27 +0100

    feck

--- Mastermind_present.py
+++ Mastermind_present.py
@@ -10,11 +10,20 @@ for i in range(4):
 for i in range(2):
     gui.rowconfigure(i+4, weight=1)
 
+
+
 backg = Label(gui, bg="grey")
 backg.grid(column=0, row=0, columnspan=10, rowspan=4,sticky="NESW")
 UI = Label(gui, bg="black")
 UI.grid(column=0, row=4, columnspan=10, rowspan=2, sticky="NESW")
 
-
+green = Button(gui, bg="dark green")
+green.grid(column=0, row=4, sticky="NESW")
+red = Button(gui, bg="dark red")
+red.grid(column=1, row=4, sticky="NESW")
+yellow = Button(gui, bg="yellow")
+yellow.grid(column=1, row=5, sticky="NESW")
+blue = Button(gui, bg="dark blue")
+blue.grid(column=0, row=5, sticky="NESW")
 
 gui.mainloop()
