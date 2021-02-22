0a8799bbff1bb2b4c0ca3668efef94cc01f2dc21
GZimmer <kristianz1000@gmail.com>
2021-02-22 10:37:15 +0100

    Added random start

--- Mastermind_Logic.py
+++ Mastermind_Logic.py
@@ -1,13 +1,30 @@
 import Mastermind_Data as data
+from random import randrange
 
 
 class dinLogik:
 
     def __init__(self):
         self.dat = data.data()
+        self.trans = {
+            0: "white",
+            1: "purple",
+            2: "Yellow",
+            3: "dark blue",
+            4: "dark red",
+            5: "dark green"
+        }
 
-    def huskStart(self, lst: list):
+    def huskStart(self, lst=None):
+
         self.dat.gemStart(lst)
+
+    def makeStart(self, num):
+        lst = []
+        for x in range(num):
+            lst.insert(self.trans.get(randrange(6)))
+        self.dat.gemStart(lst)
+
 
     def tjekFarve(self, get: list):
 
@@ -31,7 +48,6 @@ class dinLogik:
         # Første tal er rigtig placering og farve, anden tal er kun rigtig farve
         svar[1] = svar[1] - svar[0]
         return svar
-
 
 # geat = ["blå", "grøn", "blå", "rød", "lys blå"]
 # start = ["blå", "grøn", "blå", "røfa", "lygesgr"]
